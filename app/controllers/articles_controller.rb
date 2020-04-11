04127b954a701dfce021f32caa8de1df4ee68ef7
DickyMacias <macias@gmail.com>
2020-04-11 13:35:57 -0600

    Se crea controlador para articulos

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class ArticlesController < ApplicationController
+  # puede usarse para llamar las acciones repetitivas al iniciar
+  # puede hacerse con only para definir cuales, o con except para quitar
+  before_action :find_article, except: %i[new create]
+  # existe after_action para despues de ejecutar el codigo.
+
+  def show; end
+
+  def edit; end
+
+  def new
+    @article = Article.new
+  end
+
+  def update
+    @article.update(title: params[:article][:title], content: params[:article][:content])
+    redirect_to @article
+  end
+
+  def create
+    @article = Article.create(title: params[:article][:title], content: params[:article][:content])
+    render json: @article
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/bienvenida', to: 'home#index'
+
+  root to: 'home#index'
+
+  get 'articles/new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show'
+  get 'articles/:id/edit', to: 'articles#edit'
+
+  patch 'articles/:id', to: 'articles#update', as: :article
+  post 'articles', to: 'articles#create'
+
+  delete 'articles/:id', to: 'articles#destroy'
+end
