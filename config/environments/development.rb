e82f8892ca7868fdaebb2ec15b41fae9cb80c88c
DickyMacias <macias@gmail.com>
2020-04-11 15:36:15 -0600

    Se configura para el uso de Ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -59,4 +61,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Permite acceso via NGROK
+  config.hosts << ENV['NGROK_HOST'] if ENV['NGROK_HOST'].present?
 end
