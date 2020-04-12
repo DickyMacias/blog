f20f787cbb70a579a86b12fa25957a1b96a2fe23
DickyMacias <macias@gmail.com>
2020-04-12 02:34:59 -0600

    Agregar archivos o imagenes a perfil de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :articles
+  has_one_attached :avatar
 end
